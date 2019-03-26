3197a46f6ebf7f89793bc9eac3bd449b7703a596
Unknown <jakash1997@gmail.com>
2019-03-26 06:57:47 +0530

    Prediction function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # metacall-ml-example
 A repository to host ML examples for Metacall
+
+## predict_input function :
+It expects single integer or 2d array of integers as **input**, output is predicted salaries
--- loading_and_running_model.py
+++ loading_and_running_model.py
@@ -5,29 +5,28 @@
 """
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import pandas as pd
+from joblib import dump,load
+
+regressor = load('model.joblib')
 
 # Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+'''dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)'''
 
-# Loading Linear Regression Model
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor = load('model.joblib')
-
-#Predicting Results
-y_pred = regressor.predict(X_test)
+# Loading model in memory
+def predict_salary(input):
+    return regressor.predict(input)
 
 #Visualization of Training Set
-plt.scatter(X_train, y_train, color='red')
+'''plt.scatter(X_train, y_train, color='red')
 plt.plot(X_train, regressor.predict(X_train), color='blue')
 plt.title('Sal vs Exp (Training Set)')
 plt.xlabel('Yrs of Exp')
@@ -40,4 +39,4 @@ plt.plot(X_train, regressor.predict(X_train), color='blue')
 plt.title('Sal vs Exp (Test Set)')
 plt.xlabel('Yrs of Exp')
 plt.ylabel('Salary')
-plt.show()
+plt.show()'''
